34153d40988e106d2b147f65436250b4469825ec
Tjatja0 <twitch.tjatja@gmail.com>
2026-04-27 12:29:20 +0200

    Add files via upload

---
+++ Tjatja - Inventory Authorized Items/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace SpaceCraft
+{
+    public class InventoryAuthorizedItems : InventoryAuthorizedGroups
+    {
+        [SerializeField]
+        List<GroupData> _authorizedItems;
+        public void Start()
+        {
+            if (_authorizedItems == null) { return; }
+            FieldInfo FieldInfo_InventoryAuthorizedGroups__authorizedGroups = typeof(InventoryAuthorizedGroups).GetFields(BindingFlags.NonPublic | BindingFlags.Instance).Where(e => e.Name.Contains("_authorizedGroups")).First();
+            object authGroupsAsObj = FieldInfo_InventoryAuthorizedGroups__authorizedGroups.GetValue((InventoryAuthorizedGroups)this);
+            HashSet<Group> _authorizedGroups;
+            if (authGroupsAsObj == null)
+            {
+                _authorizedGroups = new HashSet<Group>();
+            }
+            else
+            {
+                _authorizedGroups = (HashSet<Group>)authGroupsAsObj;
+            }
+            foreach (var gd in _authorizedItems)
+            {
+                _authorizedGroups.Add(GroupsHandler.GetGroupViaId(gd.id));
+            }
+            FieldInfo_InventoryAuthorizedGroups__authorizedGroups.SetValue(this, _authorizedGroups);
+        }
+    }
+}
